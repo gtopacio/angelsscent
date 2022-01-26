25e572b8a0c2ce7265a5b8fbb95d2bb31abae277
Aaron Brines <aaron_brines@dlsu.edu.ph>
2022-01-26 21:27:48 +0800

    Sign-Up Test Suite Started

--- cypress/integration/checkout-tests.js
+++ cypress/integration/checkout-tests.js
@@ -1,23 +1,23 @@
 describe('Checkout Test Suits', () => {
     it('test case 1 -> luzon, less than 1 kg, P100 shipping fee', () => {
-      cy.visit(Cypress.env('test_host_url'))
+      cy.visit('http://localhost:3000/')
 
       cy.contains('SHOP NOW')
       cy.contains('SHOP NOW').trigger('mouseover').click()
 
 
       // enter home
-      cy.get('.account-header').should('have.text', '\n         Products   \n    ')
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get(':nth-child(1) > .dropdown-item').click()
       cy.wait(1000)
-      cy.get('#email').type('dummy1@gmail.com', { delay: 5})
-      cy.get('#password').type('dumdumz', { delay: 5})
+      cy.get('#login-email').type('dummy1@gmail.com', { delay: 5})
+      cy.get('#login-password').type('dumdumz', { delay: 5})
       cy.get('form > .mt-4 > .row > .btn').click()
       cy.wait(2000)
 
       // login successful for dummy1 --> check account info
-      cy.get('.account-header').should('have.text', '\n         Products   \n    ')
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get('[href="/account/info"] > .dropdown-item').trigger('mouseover').click()
       cy.get('.flex-column > .account-info > .account-section > :nth-child(1) > .section-body').should('have.text', 'dummy1')
---
+++ cypress/integration/sign-up-tests.js
@@ -0,0 +1,70 @@
+describe('Checkout Test Suits', () => {
+    it('test case 1 -> able to signed up, logged in, and log out', () => {
+      cy.visit(Cypress.env('test_host_url'))
+    
+      // check if it landed on the landing page and go to the products page
+      cy.contains('SHOP NOW')
+      cy.contains('SHOP NOW').trigger('mouseover').click()
+
+      // find the sign up button
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(2) > .dropdown-item').click()
+      cy.wait(1000)
+      
+      // fill up sign up form and register
+      cy.get('#signup-firstname').type('12345689', { delay: 5})
+      cy.get('#signup-lastname').type('12345689', { delay: 5})
+      cy.get('#signup-contactNo').type('9123456789', { delay: 5})
+      cy.get('#signup-email').type('1234689@gmail.com', { delay: 5})
+      cy.get('#signup-password').type('123456', { delay: 5})
+      cy.get('#signup-street').type('123456', { delay: 5})
+      cy.get('#signup-city').type('123456', { delay: 5})
+      cy.get('#signup-province').type('123456', { delay: 5})
+      cy.get('#signup-region').select('NCR')
+      cy.get('#signup-zipcode').type('123456', { delay: 5})
+      cy.get('#submit-signup').click()
+
+      // assert if logged in and then log out for the session
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'View Account Info')
+      cy.get(':nth-child(4) > .dropdown-item').click()
+      cy.wait(1000)
+
+      // checks if it logged out completely
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'Log In')
+    })
+
+    it('test case 2 -> able to log in and log out', () => {
+      cy.visit(Cypress.env('test_host_url'))
+
+      // check if it landed on the landing page and go to the products page
+      cy.contains('SHOP NOW')
+      cy.contains('SHOP NOW').trigger('mouseover').click()
+      
+      // find the log in button
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(1) > .dropdown-item').click()
+      cy.wait(1000)
+
+      // enter home
+      cy.get('#login-email').type('dummy1@gmail.com', { delay: 5})
+      cy.get('#login-password').type('dumdumz', { delay: 5})
+      cy.get('form > .mt-4 > .row > .btn').click()
+      cy.wait(2000)
+
+      // log out for the session
+      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(4) > .dropdown-item').click()
+      cy.wait(1000)
+
+      // checks if it logged out completely
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'Log In')
+    })
+
+})

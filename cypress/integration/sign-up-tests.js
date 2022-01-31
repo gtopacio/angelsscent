e7520209e6fb964d9a9437106829692be6834b46
Aaron Brines <aaron_brines@dlsu.edu.ph>
2022-01-31 13:21:18 +0800

    Updated Sign Up Tests

--- cypress/integration/sign-up-tests.js
+++ cypress/integration/sign-up-tests.js
@@ -1,41 +1,42 @@
-describe('Checkout Test Suits', () => {
-    it('test case 1 -> able to signed up, logged in, and log out', () => {
-      cy.visit(Cypress.env('test_host_url'))
+describe('Sign-Up Test Suite', () => {
+    // it('test case 1 -> able to signed up, logged in, and log out', () => {
+    //   cy.visit(Cypress.env('test_host_url'))
     
-      // check if it landed on the landing page and go to the products page
-      cy.contains('SHOP NOW')
-      cy.contains('SHOP NOW').trigger('mouseover').click()
+    //   // check if it landed on the landing page and go to the products page
+    //   cy.contains('SHOP NOW')
+    //   cy.contains('SHOP NOW').trigger('mouseover').click()
 
-      // find the sign up button
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
-      cy.get('#accountDropdown').trigger('mouseover').click()
-      cy.get(':nth-child(2) > .dropdown-item').click()
-      cy.wait(1000)
+    //   // find the sign up button
+    //   cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
+    //   cy.get('#accountDropdown').trigger('mouseover').click()
+    //   cy.get(':nth-child(2) > .dropdown-item').click()
+    //   cy.wait(1000)
       
-      // fill up sign up form and register
-      cy.get('#signup-firstname').type('12345689', { delay: 5})
-      cy.get('#signup-lastname').type('12345689', { delay: 5})
-      cy.get('#signup-contactNo').type('9123456789', { delay: 5})
-      cy.get('#signup-email').type('1234689@gmail.com', { delay: 5})
-      cy.get('#signup-password').type('123456', { delay: 5})
-      cy.get('#signup-street').type('123456', { delay: 5})
-      cy.get('#signup-city').type('123456', { delay: 5})
-      cy.get('#signup-province').type('123456', { delay: 5})
-      cy.get('#signup-region').select('NCR')
-      cy.get('#signup-zipcode').type('123456', { delay: 5})
-      cy.get('#submit-signup').click()
+    //   // fill up sign up form and register
+    //   cy.get('#signup-firstname').type('1234568910', { delay: 5})
+    //   cy.get('#signup-lastname').type('1234568910', { delay: 5})
+    //   cy.get('#signup-contactNo').type('9123456789', { delay: 5})
+    //   cy.get('#signup-email').type('123468910@gmail.com', { delay: 5})
+    //   cy.get('#signup-password').type('1234567', { delay: 5})
+    //   cy.get('#signup-street').type('1234567', { delay: 5})
+    //   cy.get('#signup-city').type('1234567', { delay: 5})
+    //   cy.get('#signup-province').type('1234567', { delay: 5})
+    //   cy.get('#signup-region').select('NCR')
+    //   cy.get('#signup-zipcode').type('123456', { delay: 5})
+    //   cy.get('#submit-signup').click()
 
-      // assert if logged in and then log out for the session
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
-      cy.get('#accountDropdown').trigger('mouseover').click()
-      cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'View Account Info')
-      cy.get(':nth-child(4) > .dropdown-item').click()
-      cy.wait(1000)
+    //   // assert if logged in and then log out for the session
+    //   cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
+    //   cy.get('#accountDropdown').trigger('mouseover').click()
+    //   cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'View Account Info')
+    //   cy.get(':nth-child(4) > .dropdown-item').click()
+    //   cy.wait(1000)
 
-      // checks if it logged out completely
-      cy.get('#accountDropdown').trigger('mouseover').click()
-      cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'Log In')
-    })
+    //   // checks if it logged out completely
+    //   cy.get('#accountDropdown').trigger('mouseover').click()
+    //   cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'Log In')
+    // })
+    
 
     it('test case 2 -> able to log in and log out', () => {
       cy.visit(Cypress.env('test_host_url'))
@@ -45,7 +46,7 @@ describe('Checkout Test Suits', () => {
       cy.contains('SHOP NOW').trigger('mouseover').click()
       
       // find the log in button
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get(':nth-child(1) > .dropdown-item').click()
       cy.wait(1000)
@@ -57,7 +58,7 @@ describe('Checkout Test Suits', () => {
       cy.wait(2000)
 
       // log out for the session
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get(':nth-child(4) > .dropdown-item').click()
       cy.wait(1000)
@@ -65,6 +66,67 @@ describe('Checkout Test Suits', () => {
       // checks if it logged out completely
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get(':nth-child(1) > .dropdown-item').should('have.text', 'Log In')
+    })
+
+    it('test case 4 -> Sign Up failed due to invalid email (duplciate email)', () => {
+      cy.visit(Cypress.env('test_host_url'))
+    
+      // check if it landed on the landing page and go to the products page
+      cy.contains('SHOP NOW')
+      cy.contains('SHOP NOW').trigger('mouseover').click()
+
+      // find the sign up button
+      cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(2) > .dropdown-item').click()
+      cy.wait(1000)
+      
+      // fill up sign up form and register
+      cy.get('#signup-firstname').type('1234568910', { delay: 5})
+      cy.get('#signup-lastname').type('1234568910', { delay: 5})
+      cy.get('#signup-contactNo').type('9123456789', { delay: 5})
+      cy.get('#signup-email').type('1234689@gmail.com', { delay: 5})
+      cy.get('#signup-password').type('1234567', { delay: 5})
+      cy.get('#signup-street').type('1234567', { delay: 5})
+      cy.get('#signup-city').type('1234567', { delay: 5})
+      cy.get('#signup-province').type('1234567', { delay: 5})
+      cy.get('#signup-region').select('NCR')
+      cy.get('#signup-zipcode').type('123456', { delay: 5})
+      cy.get('#submit-signup').click()
+
+      // assert if it errored dut to invalid email
+      cy.get('#invalid-email').should('have.text', 'Email is invalid.')
+ 
+    })
+
+    it('test case 3 -> Sign Up failed due to invalid email (no email included)', () => {
+      cy.visit(Cypress.env('test_host_url'))
+    
+      // check if it landed on the landing page and go to the products page
+      cy.contains('SHOP NOW')
+      cy.contains('SHOP NOW').trigger('mouseover').click()
+
+      // find the sign up button
+      cy.get('.account-header').should('have.text', '\r\n         Products\r\n    ')
+      cy.get('#accountDropdown').trigger('mouseover').click()
+      cy.get(':nth-child(2) > .dropdown-item').click()
+      cy.wait(1000)
+      
+      // fill up sign up form and register
+      cy.get('#signup-firstname').type('1234568910', { delay: 5})
+      cy.get('#signup-lastname').type('1234568910', { delay: 5})
+      cy.get('#signup-contactNo').type('9123456789', { delay: 5})
+      cy.get('#signup-password').type('1234567', { delay: 5})
+      cy.get('#signup-street').type('1234567', { delay: 5})
+      cy.get('#signup-city').type('1234567', { delay: 5})
+      cy.get('#signup-province').type('1234567', { delay: 5})
+      cy.get('#signup-region').select('NCR')
+      cy.get('#signup-zipcode').type('123456', { delay: 5})
+      cy.get('#submit-signup').click()
+
+      // assert if it errored dut to invalid email
+      cy.get('#invalid-email').should('have.text', 'Email is invalid.')
+ 
     })
 
 })

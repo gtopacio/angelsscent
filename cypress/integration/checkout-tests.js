1505ce325d880040b427b7beb3de9c41c322424a
Cherrie Pioquinto <cherrie_pioquinto@dlsu.edu.ph>
2022-02-04 20:14:31 +0800

    automation test update
    - fixed modal bugs in test
    - changed test cases for checkout tests

--- cypress/integration/checkout-tests.js
+++ cypress/integration/checkout-tests.js
@@ -1,40 +1,41 @@
 describe('Checkout Test Suits', () => {
-    it('test case 1 -> luzon, less than 1 kg, P100 shipping fee', () => {
-      cy.visit('http://localhost:3000/')
+    it('test case 1 -> NCR, less than 1 kg', () => {
+      cy.visit(Cypress.env('test_host_url'))
 
       cy.contains('SHOP NOW')
       cy.contains('SHOP NOW').trigger('mouseover').click()
 
 
       // enter home
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('.account-header').should('have.text', '\n         Products\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get(':nth-child(1) > .dropdown-item').click()
       cy.wait(1000)
-      cy.get('#login-email').type('dummy1@gmail.com', { delay: 5})
+      cy.get('#login-email').type('1dummy@gmail.com', { delay: 5})
       cy.get('#login-password').type('dumdumz', { delay: 5})
       cy.get('form > .mt-4 > .row > .btn').click()
       cy.wait(2000)
 
       // login successful for dummy1 --> check account info
-      cy.get('.account-header').should('have.text', '\r\n         Products   \r\n    ')
+      cy.get('.account-header').should('have.text', '\n         Products\n    ')
       cy.get('#accountDropdown').trigger('mouseover').click()
       cy.get('[href="/account/info"] > .dropdown-item').trigger('mouseover').click()
-      cy.get('.flex-column > .account-info > .account-section > :nth-child(1) > .section-body').should('have.text', 'dummy1')
-      cy.get(':nth-child(2) > .account-info > .account-section > :nth-child(3) > .section-body').should('have.text', 'Rizal')
+      cy.get('.flex-column > .account-info > .account-section > :nth-child(1) > .section-body').should('have.text', 'Ayato')
+      cy.get(':nth-child(2) > .account-info > .account-section > :nth-child(3) > .section-body').should('have.text', 'Manila')
 
       // return to products --> add 1 product to cart
       cy.get('[href="/products"] > .nav-link').trigger('mouseover').click()
       cy.get('#women > .d-flex > .card > a > .text-uppercase').trigger('mouseover').click()
       cy.get('.product-title').should('have.text', 'Babiel')
       cy.contains('ADD TO CART').trigger('mouseover').click()
+      cy.wait(2000)
       cy.get('.modal-body > .justify-content-center > .btn-container > .btn').trigger('mouseover').click()
-      
+      cy.wait(2000)
 
       // go to cart --> confirm if babiel is added --> proceed to checkout
       cy.get('[href="/cart"] > .nav-link').trigger('mouseover').click()
       cy.get('.text-left > :nth-child(1)').should('have.text', 'Babiel')
-      cy.get('.light').should('have.text', '50ML')
+      cy.get('.light').should('have.text', '50g')
       cy.get('.w-100 > .shadow').should('have.text', 'Proceed to Checkout').trigger('mouseover').click()
 
       // verify checkout information --> antipolo and weight is only 50 ml so shipping fee is 100
